c9a9ceb3d0667715c2ed4a05d66048def44fbe24
Elankumaran98 <siva98kumarane.com>
2023-12-19 20:19:04 +0530

    front pages created with styles and basic functions  of backend connections

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import { Route, Routes, Navigate } from "react-router-dom";
+import Profile from "./components/Profile";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 
 function App() {
+  const user = localStorage.getItem("token");
+
   return (
-    <div className="App">
-      hi
-    </div>
+    <Routes>
+      {user && <Route path="/" exact element={<Profile />} />}
+      <Route path="/signup" exact element={<Signup />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/" element={<Navigate replace to="/login" />} />
+    </Routes>
   );
 }
 
